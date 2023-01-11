b0c5a3ef94ba239389483dd2cf7abc9d4a7b27f4
Pomoc48 <lukawski.mikolaj@gmail.com>
2023-01-11 16:15:50 +0100

    Add favourite field to counter model

--- lib/models/list.dart
+++ lib/models/list.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 class ListOfItems extends Equatable {
   final int id;
   final String name;
+  final bool favourite;
   final int count;
   final DateTime timestamp;
   final List<int> chartData;
@@ -10,6 +11,7 @@ class ListOfItems extends Equatable {
   const ListOfItems({
     required this.id,
     required this.name,
+    required this.favourite,
     required this.count,
     required this.timestamp,
     required this.chartData,
@@ -19,6 +21,7 @@ class ListOfItems extends Equatable {
     return ListOfItems(
       id: map["id"],
       name: map["name"],
+      favourite: map["favourite"] == 1 ? true : false,
       count: map["count"],
       timestamp: DateTime.fromMillisecondsSinceEpoch(map["timestamp"] * 1000),
       chartData: List<int>.from(map["chart_data"]),
@@ -26,5 +29,5 @@ class ListOfItems extends Equatable {
   }
 
   @override
-  List<Object> get props => [id, name, count, timestamp, chartData];
+  List<Object> get props => [id, name, favourite, count, timestamp, chartData];
 }
